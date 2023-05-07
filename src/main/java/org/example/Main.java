f0498860f8ab0f9ee42f8e44ccd30830c4baf331
SergiyCh2023 <alien13579@ukr.net>
2023-05-07 14:14:09 +0300

    myHomework Module#2

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) throws IllegalAccessException {
+
+//        SumCalculator sumcalc = new SumCalculator();
+//        System.out.println(sumcalc.sum(1));
+//        System.out.println(sumcalc.sum(3));
+//        System.out.println(sumcalc.sum(0));
+
+    }
+
+
+
+
+}
